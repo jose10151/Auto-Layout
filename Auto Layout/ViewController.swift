44869fd8b805113d8b1d64ca3ecd5992f5cf335b
Jose Trujillo <joseh.trujillo11@gmail.com>
2024-02-14 13:10:23 -0500

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by 4d on 2/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
